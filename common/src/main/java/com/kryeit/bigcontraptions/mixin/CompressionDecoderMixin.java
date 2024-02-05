c8089a8404f4fc306512ccd477e92d30d49b519e
muriplz <46825658+muriplz@users.noreply.github.com>
2024-02-05 09:51:15 +0100

    Update CompressionDecoderMixin.java

--- common/src/main/java/com/kryeit/bigcontraptions/mixin/CompressionDecoderMixin.java
+++ common/src/main/java/com/kryeit/bigcontraptions/mixin/CompressionDecoderMixin.java
@@ -1,21 +1,22 @@
 package com.kryeit.bigcontraptions.mixin;
 
 import net.minecraft.network.CompressionDecoder;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(CompressionDecoder.class)
 public class CompressionDecoderMixin {
 
-    @ModifyArgs(method = "<init>", at = @At("HEAD"))
-    private static void modifyMaxCompressedLength(Args args) {
+    @Mutable
+    @Shadow @Final public static int MAXIMUM_COMPRESSED_LENGTH;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onConstructorReturn(CallbackInfo ci) {
         // Find the constant value in the args and modify it
-        for (int i = 0; i < args.size(); i++) {
-            if (args.get(i) instanceof Integer && (int) args.get(i) == 2097152) {
-                args.set(i, 10 * 1024 * 1024); // 10 MB
-            }
-        }
+        MAXIMUM_COMPRESSED_LENGTH = 10 * 1024 * 1024;
     }
 }
